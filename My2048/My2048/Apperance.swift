f7ccd8eef5a750338c5a99843b0e52c83d93a652
Jesell <673375312@qq.com>
2016-05-12 00:27:45 +0800

    Revert "Day2"
    This reverts commit 890945751fbe4ee94528946872ff9a8ac1b242c5.

--- My2048/My2048/Apperance.swift
+++ My2048/My2048/Apperance.swift
@@ -8,56 +8,30 @@
 
 import UIKit
 
-protocol ApperanceProtocol: class {
-    func titleColor(Value: Int) -> UIColor
-    func numberColor(value: Int)-> UIColor
-    func fontForNumbers() -> UIFont
-}
-
-class Apperance: ApperanceProtocol{
-    func func titleColor(Value: Int) -> UIColor {
-        switch Value{
-        case 2:
-            return UIColor(red: 238.0/255.0, green: 228.0/255.0,
-                       blue: 218.0/255/0, alpha:1.0)
-        case 4:
-            return UIColor(red: 237.0/255.0, green: 224.0/255.0,
-                       blue: 200.0/255/0, alpha:1.0)
-        case 8:
-            return UIColor(red: 242.0/255.0, green: 177.0/255.0,
-                       blue: 121.0/255/0, alpha:1.0)
+class Tile: UIView {
+    var delegate: AppereanceProtocol
+    var value : Int = 0 {
+        //didset 在值变化后立刻调用
+        //相对的 willset 在值变化前的调用
+        didSet {
+            //设置背景色
+            backgroundColor = delegate.titleColor(value)
+            numberLabel.textColor = delegate.numberColor(value)
+            numberLabel.text = " \(value)"
+        }
+    }
+    var numberLabel:UILabel
+    required init(coder:NSCoder)
+    {
+        fatalError("NSCoding not supported")
+    }
+    init(position:CGPoint,width:CGFloat,value:Int,radius:Cf=GL_FLOAT,delegate d:UIAppearanceProtocol){
+        addSubview(numberLabel)
+        layer.cornerRadius=radius
         
-        case 16:
-            return UIColor(red: 245.0/255.0, green: 149.0/255.0,
-                       blue: 99.0/255/0, alpha:1.0)
-        case 32:
-            return UIColor(red: 246.0/255.0, green: 124.0/255.0,
-                       blue: 95.0/255/0, alpha:1.0)
-        case 64:
-            return UIColor(red: 246.0/255.0, green: 94.0/255.0,
-                       blue: 59.0/255/0, alpha:1.0)
-        case 128, 256, 512, 1024, 2048:
-            return UIColor(red: 237.0/255.0, green: 207.0/255.0,
-                       blue: 114.0/255/0, alpha:1.0)
-        default:
-            return UIColor.whiteColor()
-        }
-    }
-    func  numberColor(value :Int) -> UIColor {
-        switch  value {
-        case 2, 4:
-            return UIColor(red: 119.0/255.0, green: 110.0/255.0,
-                           blue: 101.0/255/0, alpha:1.0)
-        default:
-            return UIColor.whiteColor()
-        }
-    }
-    func fontFornumbers(value: Int) -> UIFont {
-        if let font = UIFont(name: "HelveticaNeue-Bold", size: 20)
-            {
-                return font
-            }
-            return UIFont.systemFontOfSize(20)
+        self.value=value
+        backgroundColor=delegate.tileColor(value)
+        numberLabel.textColor=delegate.numberColor(value)
+        numberLabel.text="\(value)"
     }
 }
-
