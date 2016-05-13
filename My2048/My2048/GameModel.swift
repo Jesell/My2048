3db93463f882c9b89f8cc70945ed13e145ec7840
Jesell <673375312@qq.com>
2016-05-14 00:27:01 +0800

    day-4
    GameModel （TAT）

--- My2048/My2048/GameModel.swift
+++ My2048/My2048/GameModel.swift
@@ -6,4 +6,52 @@
 //  Copyright © 2016年 Jesell. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+//主要是算法的实现(移动合并算法)
+
+protocol  GameModelProtocol : class {
+    func scoreChange(score: Int)
+    func moveOneTtile(from: (Int, Int), to: (Int, Int), value: Int)
+    func moveTwoTile(from: ((Int, Int), (Int, Int)), to: (Int, Int), value: Int)
+    func insertTitle(location: (Int, Int), value: Int)
+ }
+
+class GameModel: NSObject {
+    let dimensipon: Int
+    let threshold: Int
+
+    
+    var score: Int = 0 {
+        didSet {
+            delegate.scoreChanged(score)
+        }
+    }
+    
+    var gameBoard: SquareGameBoard<TitleObject>
+    
+    let delegate: GameModelProtocol
+    
+    var queue: [moveCommand]
+    var timer: NSTimer
+    
+    let maxCommands = 100
+    let queueDelay = 0.3
+    
+    init(dimension d: Int, threshold t: Int, delegate: GameModelProtocol) {
+        dimensipon = d
+        threshold = t
+        self.delegate = delegate
+        queue = [moveCommand]()
+            timer = NSTimer()
+        gameboard = SquareGameboard(dimensipon: d, initvalue: .Empty)
+        super.init()
+    }
+    
+    func respect() {
+        score = 0
+        gameboard.setAll(.Empty)
+        queue.removeAll(keepCapacity: true)
+        timer.invalidate()
+}
+}
