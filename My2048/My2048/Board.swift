e68e3ed07ca1bbad68c9c2cf3b932074b99c7e90
Jesell <673375312@qq.com>
2016-05-16 23:57:27 +0800

    My2048

--- My2048/My2048/Board.swift
+++ My2048/My2048/Board.swift
@@ -18,8 +18,37 @@ class Board : UIView {
     
     let tilePoStartScale: CGFloat = 1.0
     let tileMergeExpandTime: NSTimeInterval= 0.08
-    let tileMergeContractTime: NSTimeInterval
+    let tileMergeContractTime: NSTimeInterval = 0.08
     
+    let perSquareSlideDuration: NSTimeInterval = 0.08
+    
+    init(dimension d: Int, tilewidth width: CGFloat, tilePadding padding: CGFloat, cornRadius radius: CGFloat, backgroundColor: UIColor, foregroundColor: UIColor){
+        assert(d>0)
+        dimension=d
+        tileWidth=width
+        tilePadding=padding
+        cornerRadius=radius
+        tiles=Dictionary()
+        let sideLength = padding + CGFloat(dimension)*(width + padding)
+        super.init(frame: CGRectMake(0, 0, sideLength, sideLength))
+        layer.cornerRadius = radius
+        setupBackground(backgroundColor: backgroundColor, tileColor: foregroundColor)
+        
+    }
+    
+    required init(coder: NSCoder){
+        fatalError("NSCoding not supported")
+    }
+    
+    //重新设置游戏面板
+    func reset(){
+        for (key, tile) in tiles {
+            tile.removeFromSuperview()
+        }
+        tiles.removeAll(keepCapacity: true)
+    }
+    
+    //
 }
 
 
